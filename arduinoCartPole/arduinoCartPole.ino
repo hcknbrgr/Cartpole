9275a6a14136debea58bf250323bc4b433b3d4de
hcknbrgr <hcknbrgr@gmail.com>
2023-04-04 20:56:32 -0400

    Cart centered check

--- arduinoCartPole/arduinoCartPole.ino
+++ arduinoCartPole/arduinoCartPole.ino
@@ -1,65 +1,139 @@
-/*
+  //TODO: CENTER THE CART IN THE ENVIRONMENT TO ENSURE MOTOR CONTROL WORKS
 
+#include <MPU6050.h>
+#include <Wire.h>
+
+/*
   Ultrasonic sensor pins:
     VCC: +5VDC
     Trig : Trigger (Input) - Pin11
     Echo: Echo (OUtput) - Pin12
     GND: GND
   
+  MPU 6050 gyroscope pins:
+    VCC: +5VDC
+    GND: GND
+    SCL: SCL
+    SDA: SDA
 
+  Motor pinout
+    Motor1
+      OUT1: Pin1
+      OUT2: Pin2
+      ENA: PWMPin3
+    Motor2
+      OUT3: Pin7
+      OUT4: Pin8
+      ENB: PWMPin6
 */
 
+// MOTOR PINS
+int motor1pin1 = 1;
+int motor1pin2 = 2;
+int motor1PWM = 3;
+int motor2pin1 = 7;
+int motor2pin2 = 8;
+int motor2PWM = 6;
+int boardLED = 13;
+
+// ULTRASONIC SENSOR PINS
 int trigPin = 11;   // Trigger
 int echoPin = 12;   // Echo
-int LEDPin = 13;    // LED
-int validDistance = 0; // Serial Input to determine valid
 
-long duration, cm, inches;
+MPU6050 mpu;
+
+int speed = 0;  // 0 - 255
+int motorDirection = 0; // Serial Input to determine which direction to send the motor in. 
+  // 0 = move towards sensor
+  // 1 = stationary
+  // 2 = move away from sensor
+
+long duration;
 
 void setup() {
-  // put your setup code here, to run once:
   // Serial Port begin
-  Serial.begin (9600);
-  //Define inputs and outputs
+  Serial.begin (115200);
+  digitalWrite(boardLED, LOW);  
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println(0);
+    delay(500);
+  }
+  pinMode(boardLED, OUTPUT);
+  digitalWrite(boardLED, HIGH);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(LEDPin, OUTPUT);
-  digitalWrite(LEDPin, LOW);
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor1PWM, OUTPUT); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse;
-
+  
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-
-  // Read the signal from the sensor: a HGIH pulse whose duration is the time from the sending of the ping to the reception of its echo off of an object.
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
+  Serial.println(duration);  
+  //cm = (duration/2) / 29.1;   lets do this math on the laptop
+  //send over serial and let python do the math
 
-  // Convert the time into a distance
-  cm = (duration/2) / 29.1;   
-  inches = (duration/2) / 74;
-
-  Serial.println(cm);
-  if(Serial.available()>0){
-    validDistance = Serial.read();
+  delay(25); // delay to let serial communication happen
+  
+  if(Serial.available()>0) {
+    motorDirection = Serial.read(); 
   }
-  if(validDistance == 1)
+  // 0 = move towards sensor
+  // 1 = stationary
+  // 2 = move away from sensor
+
+  // if we want to use the PWM to modify the speed, we'll use: analogWrite(motor1PWM, speed);
+  // we need to assign speed
+  if(motorDirection == 0)  // 0 = move towards sensor
   {
-    digitalWrite(LEDPin, HIGH);
+    digitalWrite(motor1pin1, HIGH);
+    digitalWrite(motor1pin2, LOW);
+    digitalWrite(motor2pin1, LOW);
+    digitalWrite(motor2pin2, HIGH);
+  } 
+  else if(motorDirection == 2)
+  {
+    digitalWrite(motor1pin1, HIGH);
+    digitalWrite(motor1pin2, LOW);
+    digitalWrite(motor2pin1, LOW);
+    digitalWrite(motor2pin2, HIGH);
   }
-  else
-    digitalWrite(LEDPin, LOW);
+  else if(motorDirection == 3)  // 3 = this is set ready to go!
+  {
+    digitalWrite(boardLED, LOW);  // turn off the light that was turned on when the gyroscope was online
+  }
+  else  // 1 = stationary
+  {
+    digitalWrite(motor1pin1, LOW);
+    digitalWrite(motor1pin2, LOW);
+    digitalWrite(motor2pin1, LOW);
+    digitalWrite(motor2pin2, LOW);
+  }
 
+/*
+  // *********** Code for pitch and roll
+  // Read normalized values
+  Vector normAccel = mpu.readNormalizeAccel();
 
+  // Calculate Pitch & ROll by Korneliusz Jarzebski
+  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
+  Serial.println(duration);
 
+  //int roll = (atan2(normAccel.YAxis, normAccel.ZAxis)*180.0)/M_PI;
+  
+  Serial.println(pitch);
+*/  
 
-  delay(250);
+  delay(100);
   
 }
--- cartpole.py
+++ cartpole.py
@@ -3,20 +3,35 @@ import time
 import struct  # for sending data as bytes
 import matplotlib.pyplot as plt
 
-arduino = serial.Serial('COM7', 9800, write_timeout=0.5, timeout=.5)
-time.sleep(2)
+# TODO: INITIALIZE CART DISTANCE
+# TODO: INITIALIZE VERTICAL ON SENSOR
+# TODO: MARK START OF EPISODE -- TURN ON THE BOARD LIGHT
+# TODO: MARK END OF EPISODE
+# TODO: IMPLEMENT REINFORCEMENT ALGORITHM
 
+# Test the cart distance for initialization.
+arduino = serial.Serial('COM7', 115200, write_timeout=0.5, timeout=.5)
+time.sleep(2)
+cartCenteredCounts = 0
 data = []
-for i in range(50):
+while cartCenteredCounts >= 100:
     line = arduino.readline()
     if line:
         string = line.decode()
         num = int(string)
-        print(num)
-        if num > 12:
-            arduino.write(struct.pack('>B', 1))
-        else:
+        cm = (num / 2.0) / 29.1
+        if num < 8.0:
+            arduino.write(struct.pack('>B', 2))
+            cartCenteredCounts = 0
+        elif num > 9.0:
             arduino.write(struct.pack('>B', 0))
+            cartCenteredCounts = 0
+        else:
+            cartCenteredCounts += 1
+            if cartCenteredCounts >= 100:
+                arduino.write(struct.pack('>B', 3))
+            else:
+                arduino.write(struct.pack('>B', 1))
         data.append(num)
 
 arduino.close()
